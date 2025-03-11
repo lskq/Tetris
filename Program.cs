ad6f8fd8fa6c000cf16f2f6253deb953bd591e01
lskq <lskq012025@gmail.com>
2025-03-11 17:43:24 +0100

    Update ConsoleView visuals

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ class Program
         catch (Exception e)
         {
             // For testing
+            Console.Write("\x1b[0m");
             Console.Clear();
             Console.CursorVisible = true;
             Console.WriteLine(e.ToString());
--- View/ConsoleView.cs
+++ View/ConsoleView.cs
@@ -4,8 +4,7 @@ namespace Tetris.View;
 
 public class ConsoleView
 {
-    public const char BlockChar = '#';
-    public const char EmptyChar = ' ';
+    public const char Space = ' ';
 
     public Game Game { get; set; }
     public bool[,] Grid { get; set; }
@@ -16,13 +15,14 @@ public class ConsoleView
         Grid = game.Grid;
 
         Console.CursorVisible = false;
+        Console.Clear();
     }
 
     public void Step()
     {
         Tetromino tetromino = Game.Tetromino;
 
-        string colorCode = GetColorCode(tetromino.Color);
+        string emptyString = "\x1b[0m" + Space;
 
         for (int x = 0; x < Game.Width; x++)
         {
@@ -31,10 +31,12 @@ public class ConsoleView
                 if (!Grid[x, y])
                 {
                     Console.SetCursorPosition(x, y);
-                    Console.WriteLine(EmptyChar);
+                    Console.Write(emptyString);
                 }
             }
         }
+
+        string blockString = GetColorCode(tetromino.Color) + Space;
 
         foreach ((int, int) block in tetromino.Blocks)
         {
@@ -42,7 +44,7 @@ public class ConsoleView
             int y = tetromino.Y + block.Item2;
 
             Console.SetCursorPosition(x, y);
-            Console.WriteLine(colorCode + BlockChar);
+            Console.Write(blockString);
         }
     }
 
@@ -50,13 +52,13 @@ public class ConsoleView
     {
         return color switch
         {
-            Color.Cyan => "\x1b[0;255;255m",
-            Color.Blue => "\x1b[0;0;255m",
-            Color.Orange => "\x1b[255;165;0m",
-            Color.Yellow => "\x1b[255;255;0m",
-            Color.Green => "\x1b[0;255;0m",
-            Color.Purple => "\x1b[255;0;255m",
-            Color.Red => "\x1b[255;0;0m",
+            Color.Cyan => "\x1b[48;2;0;255;255m",
+            Color.Blue => "\x1b[48;2;0;0;255m",
+            Color.Orange => "\x1b[48;2;255;165;0m",
+            Color.Yellow => "\x1b[48;2;255;255;0m",
+            Color.Green => "\x1b[48;2;0;255;0m",
+            Color.Purple => "\x1b[48;2;255;0;255m",
+            Color.Red => "\x1b[48;2;255;0;0m",
             _ => "\x1b[39;49m",
         };
     }
